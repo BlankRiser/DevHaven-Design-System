8fbad91308f6ad8be955a1c573e34ae5003b9f88
BlankRiser <rschoudhary1999@gmail.com>
2022-03-18 10:33:26 +0530

    :zap: design system starter

---
+++ components/Button/Button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Button() {
+  return <button className="p-4 bg-red-50 font-mono">Button</button>;
+}
---
+++ components/index.ts
@@ -0,0 +1,2 @@
+export {Button, ButtonSecondary} from './Button';
+export {Search} from './Search';
